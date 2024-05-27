857915cf9c428ceec35b6736676f42415aa7df1d
Siddharth Jain <siddharthjainj25@gmail.com>
2024-05-27 23:33:16 +0530

    Fixed alignment of posts on home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,11 +25,11 @@ export default function Home() {
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='max-w-8xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-            <div className='flex flex-wrap gap-4'>
+            <div className='flex flex-wrap justify-center gap-3'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
